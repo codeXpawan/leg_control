19df97b420d28ad395c12338138781d5713d8cc4
codexpawan <shahpawan56@gmail.com>
2025-12-18 22:52:00 +0545

    Updated the node tuner

--- src/ros2_jazzy/setup.py
+++ src/ros2_jazzy/setup.py
@@ -31,7 +31,7 @@ setup(
             'oslsim_controller = ros2_jazzy.controller:main',
             'loadcell = ros2_jazzy.loadcell:main',
             'oslsim_walker = ros2_jazzy.walk:main',
-            # 'oslsim_pid_tuner = ros2_jazzy.tuner:main',
+            'oslsim_pid_tuner = ros2_jazzy.tuner:main',
         ],
     },
 )
