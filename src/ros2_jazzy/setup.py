ad0c89f68785e34404f6409652dc70f716b95906
codexpawan <shahpawan56@gmail.com>
2025-12-18 21:30:53 +0545

    Updated code for the controller node

---
+++ src/ros2_jazzy/setup.py
@@ -0,0 +1,37 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'ros2_jazzy'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'data'),
+            glob('data/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='pika',
+    maintainer_email='pika@todo.todo',
+    description='TODO: Package description',
+    license='MIT',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'oslsim_controller = ros2_jazzy.controller:main',
+            # 'loadcell = ros2_jazzy.loadcell:main',
+            # 'oslsim_walker = ros2_jazzy.walk:main',
+            # 'oslsim_pid_tuner = ros2_jazzy.tuner:main',
+        ],
+    },
+)
