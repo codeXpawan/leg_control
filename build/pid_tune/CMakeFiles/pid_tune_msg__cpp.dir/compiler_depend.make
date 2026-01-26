29bb10028018af5a1da76ab14145698345c87dad
Hari <077bme053.hari@pcampus.edu.np>
2026-01-26 16:43:59 +0545

    from my side

--- build/pid_tune/CMakeFiles/pid_tune_msg__cpp.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for pid_tune_msg__cpp.
-# This may be replaced when dependencies are built.
--- build/ros2_jazzy/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/hari-prasad-gajurel/Our_control/leg_control/install/pid_tune/share/pid_tune/package.sh"
